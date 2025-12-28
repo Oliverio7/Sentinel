2e24d7fc9afb9541534d14aca0311d74368dfc6f
Oliverio D <oliveriodbs@gmail.com>
2025-12-27 20:57:47 -0600

    add name slash command

--- commands/name.js
+++ commands/name.js
@@ -1,8 +1,17 @@
+const { SlashCommandBuilder } = require("discord.js");
+
 module.exports = {
-  name: "name",
-  description: "Replies with the bot's name",
-  execute(message, args) {
-    const botName = message.client.user.username;
-    message.reply(`My name is ${botName}`);
+  // 1. DEFINITION
+  data: new SlashCommandBuilder()
+    .setName("name")
+    .setDescription("Replies with the bot's name"),
+
+  // 2. EXECUTION
+  async execute(interaction) {
+    // Access the bot's user object through the interaction client
+    const botName = interaction.client.user.username;
+
+    // Reply to the user
+    await interaction.reply(`My name is ${botName}`);
   },
 };
